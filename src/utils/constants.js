a807fafe0c992d5162e3bc59e770af4c1b3ffaff
youngineer <kartik.s@learner.manipal.edu>
2025-06-30 12:26:23 +0530

    fix: dynamic baseurl loading

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,7 @@
-export const BASE_URL = "/api";
+export const BASE_URL = 'production' 
+  ? '/api'  // production
+  : 'http://localhost:3000/api';  // development
+
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/kartikyes/';
 export const GITHUB_ICON_URL = 'https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png';
 export const GITHUB_REPO_URL = 'https://github.com/youngineer/GitTogether';
